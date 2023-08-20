c5a946c5977760c8eb4ca42547d3f3417511fb08
Ellie Korth <87391629+ellsk21@users.noreply.github.com>
2023-08-20 12:43:10 +1200

    added bootstrap and updated react

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project is by Ellie Korth and is open source coded on <a href="https://github.com/ellsk21/react-project" target="_blank">Github</a></footer>
+        </div>
     </div>
 
   );
